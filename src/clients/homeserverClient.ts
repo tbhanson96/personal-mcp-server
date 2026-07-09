156ad4923e82e5675fdaea11b7e85ad8bc165f08
Tim Hanson <tbh20715@gmail.com>
2026-07-08 17:13:39 -0700

    Use homeserver API routes

--- src/clients/homeserverClient.ts
+++ src/clients/homeserverClient.ts
@@ -4,18 +4,18 @@ export class HomeserverClient {
   constructor(private readonly http: HttpClient) {}
 
   listFiles(path = '/'): Promise<unknown> {
-    return this.http.get('/files', { path });
+    return this.http.get('/api/files/path', { path });
   }
 
   listEbooks(library = 'books'): Promise<unknown> {
-    return this.http.get('/ebooks', { library });
+    return this.http.get('/api/ebooks', { library });
   }
 
   searchEbooks(search: string): Promise<unknown> {
-    return this.http.get('/ebooks/search', { search });
+    return this.http.get('/api/ebooks/libgen', { search });
   }
 
   getHealthCatalog(): Promise<unknown> {
-    return this.http.get('/health/catalog');
+    return this.http.get('/api/health/catalog');
   }
 }
